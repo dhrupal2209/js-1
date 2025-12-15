7e527caff881a0da2ac02ae3b48edc8346480511
Dhrupal Patel <123295095+dhrupal2209@users.noreply.github.com>
2025-12-15 11:06:37 +0000

    Datatypes

---
+++ JS/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = "156789"
+var accountEmail = "dhrupal@gmail.com"
+let accont_Password = "124@56"
+account_city = "ahmedabad" 
+
+accont_Password = "1253"
+accountEmail ="Hdfc@gmail.com"
+account_city = "mehsana"
+
+
+let account_State;
+
+// accountId = 2 we don't give Assignment operator to const variable
+console.log(accountId);
+console.log(accountEmail);
+console.log(accont_Password);
+
+console.table([accountId,accountEmail,accont_Password,account_city,account_State]);
+
+
---
+++ JS/02_Datatypes.js
@@ -0,0 +1,19 @@
+//alert(3+3)
+
+//types of Datatypes
+
+let name = "Dhrupal" //string
+let age = 21         //number
+// BigInt => big number Assign
+// Boolean => True/False
+//Undefined
+//null
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
+ 
