de1f43f9d32b4e600ccb1b31c729cea25b9fb1dd
Dhrupal Patel <123295095+dhrupal2209@users.noreply.github.com>
2025-12-18 04:05:35 +0000

    Conversionoperation done

---
+++ JS/ConversionOperation.js
@@ -0,0 +1,46 @@
+let score = "hitesh"
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+let IsLoggedIn = "hitesh"
+let IsBoleanInLoggedIn = Boolean(IsLoggedIn)
+console.log(IsBoleanInLoggedIn);
+
+//1 => true 0 => false
+let anyNumber = 44
+let numberString = String(anyNumber)
+console.log(numberString);
+console.log(typeof(numberString));
+
+//operation
+let str1 = "Hello "
+let str2 = "Dhrupal"
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2%3); //remainder
+
+let number = 3
+let negNumber = -number 
+console.log(negNumber);
+
+
+let num = 5;
+num++;
+++num;
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
