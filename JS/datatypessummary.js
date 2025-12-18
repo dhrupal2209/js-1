ceb031dd49fa1ddc431a6aee008e4a19fb552c92
Dhrupal Patel <123295095+dhrupal2209@users.noreply.github.com>
2025-12-18 05:53:03 +0000

    datatypes summary done

---
+++ JS/datatypessummary.js
@@ -0,0 +1,38 @@
+/*there is 2 types of datatypes
+//1.primitive data types
+//2.non-primitive datatypes
+//list of primitive
+1.String
+2.Number
+3.Boolean
+4.Null
+5.Undefined
+6.BigInt
+7.Symbol
+
+non primitive
+1.Function
+2.object
+3.Array*/
+const score = 21
+const Scorevalue = 22.9
+const name = "hitesh"
+const IsLoggedIn = false
+const outSideTemp = null
+let useremail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(typeof(score));
+console.log(typeof name);
+console.log(typeof IsLoggedIn);
+console.log(typeof outSideTemp);
+console.log(typeof useremail);
+console.log(typeof id);
+console.log(typeof anotherId);
+
+
+
+
+
+
